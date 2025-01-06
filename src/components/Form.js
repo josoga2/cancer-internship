716107d2d1f5b09aee754c199a58b1c97548a242
josoga2 <josoga2@gmail.com>
2025-01-06 01:39:15 +0100

    fix navbar login

--- src/components/Form.js
+++ src/components/Form.js
@@ -74,7 +74,7 @@ const Form = ({ isLogin = true }) => {
     <div>
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 ">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img className='items-center w-full  h-12' src={smallLogo} />
+        <a href='/'><img className='items-center w-full  h-12' src={smallLogo} /></a>
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           {isLogin ? 'Sign in to your account' : 'Create a new account'}
         </h2>
