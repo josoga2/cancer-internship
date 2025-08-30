bc714ad8b8b4a5c4997f64aba32ad898a362112a
josoga2 <josoga2@gmail.com>
2025-08-30 19:51:02 +0200

    py_r

--- src/app/dashboard/pathway/courses/[courseId]/module/[moduleId]/content/[contentId]/page.tsx
+++ src/app/dashboard/pathway/courses/[courseId]/module/[moduleId]/content/[contentId]/page.tsx
@@ -891,7 +891,7 @@ return (
                                                 <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                                                 <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
                                             </svg>
-                                        ): (<p>Your grade is: {grade}</p>)}
+                                        ): (<p>Your grade is: {grade} XP</p>)}
 
                                     </div>
                                 </div>
