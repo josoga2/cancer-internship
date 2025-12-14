e13a12bce257d84df76f518de00ab450d3443173
josoga2 <josoga2@gmail.com>
2025-12-14 21:25:36 +0100

    cert-clean-temp

--- src/app/internship/page.tsx
+++ src/app/internship/page.tsx
@@ -259,7 +259,7 @@ export default function Page() {
                     </div>
                 </div>
                 
-                </div>
+            </div>
 
         </main>
     <Footer />
